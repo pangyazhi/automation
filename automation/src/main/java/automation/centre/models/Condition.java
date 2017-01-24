e69a0a5e7b0f969b4750303f1090822aa03634d2
jien.huang <huangjien@gmail.com>
2017-01-24 14:07:15 +0000

    remove classes

--- automation/src/main/java/automation/centre/models/Condition.java
+++ automation/src/main/java/automation/centre/models/Condition.java
@@ -8,6 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
  */
 @Document(collection = "models")
 public class Condition extends TestScript {
+
     private String condition;
 
     public String getCondition() {
