41661c826394f0d830020b12457c32ed9b445cf8
jien.huang <huangjien@gmail.com>
2017-02-03 15:55:17 +0000

    something in trouble: http get local file failed

--- auto/src/app/app.component.ts
+++ auto/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Search Address</h1>
+  <input ng2-auto-complete [(ngModel)]="searchStr" [source]="googleGeoCode" display-property-name="formatted_address"
+  placeholder="Enter Address(min. 2 chars)" min-chars="2" path-to-data="results" />
+            `
 })
+// <ng2-completer [(ngModel)]="searchStr" [dataService]="dataService" 
+//             [minSearchLength]="0"  ></ng2-completer>
 export class AppComponent {
-  title = 'app works!';
+  searchStr: string;
+  searchData = [
+    'red', 'blue', 'black'
+  ];
+  googleGeoCode: string = 'https://maps.googleapis.com/maps/api/geocode/json?address=:keyword';
+
 }
--- auto/src/app/app.module.ts
+++ auto/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { Ng2CompleterModule } from "ng2-completer/ng2-completer.module";
 import { AppComponent } from './app.component';
+import { Ng2AutoCompleteModule } from 'ng2-auto-complete';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    Ng2CompleterModule,
+    Ng2AutoCompleteModule,
     HttpModule
   ],
   providers: [],
