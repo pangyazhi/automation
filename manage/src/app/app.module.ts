23bc31a7431a3b39e09e6368f065217aa8344371
jhuang <huangjien@gmail.com>
2017-05-18 10:40:31 +0100

    add a little bit to manage

--- manage/src/app/app.module.ts
+++ manage/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DynamicComponent } from './dynamic/dynamic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicComponent
   ],
   imports: [
     BrowserModule,
